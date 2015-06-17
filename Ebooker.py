b5bc1f62f3bc014ecdfcba560215effed2377394
Etienne Ott <eti.ott@gmail.com>
2015-06-17 21:01:39 +0200

    Implemented very crude parameterization for repeating units of params.

--- Ebooker.py
+++ Ebooker.py
@@ -91,6 +91,39 @@ def apply_params(raw_text, config):
         text = text.replace(old, replacements[old])
 
     # Second, replace iterated params
+    pattern = re.compile(r'<%\+(.*)%>(.*)(<%-\1%>)', re.DOTALL)
+    matches = pattern.finditer(text)
+    replacements = []
+
+    if matches:
+        for match in matches:
+            parts = match.group(1).split('.')
+            units = []
+            if len(parts) == 1:
+                key = parts[0]
+                if key in config:
+                    units = config[key]
+                elif GLOBAL_NAMESPACE in config and key in config[GLOBAL_NAMESPACE]:
+                    units = config[GLOBAL_NAMESPACE][key]
+            elif len(parts) == 2:
+                ns = parts[0]
+                key = parts[1]
+                if ns in config and key in config[ns]:
+                    units = config[ns][key]
+
+            tmp = ""
+            for unit in units:
+                params = config[unit]
+                unit_text = match.group(2)
+                for param_key in params:
+                    unit_text = unit_text.replace('<%~' + param_key + '%>', params[param_key])
+                tmp += unit_text
+            replacements.append({
+                "old": match.group(0),
+                "new": tmp,
+            })
+        for repl in replacements:
+            text = text.replace(repl["old"], repl["new"])
 
     return text
 
@@ -133,7 +166,6 @@ def get_config_for_session(id):
 
 # Now, do the thing
 config = get_config_for_session("0")
-print config
 
 # Clone skeleton and parameterize static params
 if not os.path.exists("sessions/0/" + config["book"]["name"]):
@@ -156,3 +188,4 @@ for chapterFilename in config["book"]["chapters"]:
     with open("sessions/0/" + chapterFilename) as chapterFile:
         output = apply_params(chapterFile.read(), config)
         chapters.append(output)
+        print output
