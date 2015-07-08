965bb63ff23218c78cf1350f03dc3fd5d2c2f9ab
Etienne Ott <eti.ott@gmail.com>
2015-07-08 08:28:55 +0200

    Add basic form structure to index file.

--- index.py
+++ index.py
@@ -25,9 +25,36 @@ print """
 </head>
 
 <body>
-    <h3> featherlib ebooker </h3>
+    <h1>featherlib ebooker</h1>
+    <h2>Make epubs in one simple form.</h2>
 
-    <p>Make epub with a simple form.</p>
+    <p>
+        Here's how:
+        <ol>
+            <li>Add title, author and optionally additional information about the document</li>
+            <li>Paste the text for each chapter into the chapter input fields</li>
+            <li>Click <i>Make epub</i>, then wait for the file dialog to save your epub</li>
+        </ol>
+    </p>
+
+    <form id="docForm" method="post" action="index.py">
+        <label for="doc-name">Document name</label><br/>
+        <input type="text" name="doc-name" id="doc-name"/><br/>
+
+        <label for="doc-author">Author</label><br/>
+        <input type="text" name="doc-author" id="doc-author"/><br/>
+
+        <div class="chapter">
+            <label for="chapter-1">Chapter 1</label><br/>
+            <input type="text" name="chapter-1-name" id="chapter-1-name"/><br/>
+            <textarea name="chapter-1-content" id="chapter-1-content"></textarea>
+        </div>
+
+        <button class="addChapter" onClick="addChapter()">Add chapter</button><br/>
+
+        <button type="submit" name="save" id="save">Save</button>
+        <button type="submit" name="create" id="create">Create epub</button>
+    </form>
 
 </body>
 </html>
